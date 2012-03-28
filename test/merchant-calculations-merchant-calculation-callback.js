508e99b5cd6a988768e7e8f4fa3b4cfb2cba85e8
Dan VerWeire <dverweire@gmail.com>
2012-03-28 18:35:03 -0400

    test: process each address in teh merchantCalculationCallback test

--- test/merchant-calculations-merchant-calculation-callback.js
+++ test/merchant-calculations-merchant-calculation-callback.js
@@ -1,11 +1,46 @@
 var GoogleCheckout = require('../').GoogleCheckout,
 	common = require('./common'),
 	path = require('path'),
-	fs = require('fs')
+	fs = require('fs');
 
 var gc = new GoogleCheckout(common.settings);
 
 gc.MerchantCalculations.on('merchantCalculationCallback', function (obj, callback) {
+	obj.addresses.forEach(function (address) {
+		if (address.shippingName) {
+			//TODO look up the rate information for the address for shippingName shipping
+			
+			address.shippingRate = 14.50;
+			address.shippable = true;
+		}
+		
+		if (address.taxTotal) {
+			//look up the tax rate for the address and then calculate a total
+			address.taxTotal = 4.50;
+		}
+		
+		if (address.merchantCodes) {
+			//loop through each merchant code and validate it for this address
+			addresss.merchantCodes.forEach(function (merchantCode) {
+				//look up merchantCode.code somewhere
+				
+				//if you are using this as an example, you would need to do your
+				//own magic to determine if the code is a gift-certificate or
+				//a coupon. 
+				if (/GiftCert/.test(merchant.code) {
+					merchantCode.type = 'gift-certificate';
+				}
+				else {
+					merchantCode.type = 'coupon';
+				}
+				
+				merchantCode.valid = true;
+				merchantCode.calculatedAmount = 5.00;
+				merchantCode.message = 'You saved $5.00, good for you!!';
+			});
+		}
+	});
+	
 	return callback(null, obj);
 });
 
